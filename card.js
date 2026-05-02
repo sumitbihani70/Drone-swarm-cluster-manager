d14b70982b7f4ba8a7e46ab11b59b7c8d0e7abb3
sumitbihani70 <241353478+sumitbihani70@users.noreply.github.com>
2026-05-02 15:01:23 +0530

    Made drag and drop box put icon now more html is remaing then after done with html i will do start with css

---
+++ card.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Run after HTML is loaded
+document.addEventListener("DOMContentLoaded", function () {
+
+  // Convert all <i data-lucide="..."> into icons
+  lucide.createIcons();
+
+});
